2ab809ee19d1b2710dd659e6f2e2042021077436
wangmaiqiang <1714829759@qq.com>
2020-09-03 18:54:11 +0800

    登录注册

---
+++ assets/js/login.js
@@ -0,0 +1,66 @@
+$(function () {
+    // 点击“去注册账号”的链接
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+
+    // 点击“去登录”的链接
+    $('#link_login').on('click', function () {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+
+    var form = layui.form;
+
+    form.verify({
+        // 自定义了一个叫做 pwd 校验规则
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        // 校验两次密码是否一致的规则
+        repwd: function (value) {
+            // 通过形参拿到的是确认密码框中的内容
+            // 还需要拿到密码框中的内容
+            // 然后进行一次等于的判断
+            // 如果判断失败,则return一个提示消息即可
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致！'
+            }
+        }
+    })
+
+    var layer = layui.layer;
+    $("#form_reg").on("submit", function (e) {
+        e.preventDefault();
+        // console.log(123);
+        var data = {
+            username: $("#form_reg [name = username]").val(),
+            password: $("#form_reg [name = password]").val(),
+        }
+        $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (res) {
+            console.log(res);
+            if (res.status !== 0) {
+                return layer.msg(res.message);
+            }
+            layer.msg("注册成功");
+        })
+    })
+
+    $("#form_login").on("submit", function (e) {
+        e.preventDefault();
+        $.ajax({
+            method: "post",
+            url: "http://ajax.frontend.itheima.net/api/login",
+            data: $(this).serialize(),
+            success: function (res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                layer.msg(res.message);
+                localStorage.setItem("token", res.token);
+                location.href = "/index.html";
+            }
+        })
+    })
+})
